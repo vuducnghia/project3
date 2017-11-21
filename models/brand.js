b159d749450f42e74addf5484dff968bca806db0
Huu Nam <nam@gmail.com>
2017-11-21 08:28:53 +0700

    demo cau truc don gian

---
+++ models/brand.js
@@ -0,0 +1,17 @@
+var db = require('./db');
+
+var data = {
+    list: function(){
+		db.query('SELECT * FROM brand', function(err, results) {
+		    if (err) throw err
+
+		    console.log('aaaaaaaaaaaa');
+		    console.log('aaaaaaaaaaaa');
+			
+		    return results[0];
+		});
+    }
+};
+
+console.log(data.list());
+module.exports = data;
---
+++ models/namemodel.js
@@ -0,0 +1,5 @@
+var data = {
+    name: 'Huu Nam'
+};
+
+module.exports = data;
