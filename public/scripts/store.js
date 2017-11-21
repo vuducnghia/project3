2aed47974d5342ab5d91ada5d9908bebd3e37c36
thangbk2209 <thangbk2209@gmail.com>
2017-11-21 18:55:28 +0700

    hoàn thành sign up khách hàng

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -16,3 +16,30 @@ console.log('start');
            
          
         })
+         app.controller('sign-up', function ($http,$scope,$window) {
+            
+            $scope.register = function () {
+                console.log('lol');
+                console.log($scope);
+                var data = {
+                    username : $scope.name,
+                    email: $scope.email,
+                    password: $scope.password
+                };
+
+                console.log(data);
+                console.log(data);
+                $http({
+                    method: "POST",
+                    url: "users/signup",
+                    data: data
+                }).success(function (data) {
+                    
+                    console.log("signup successful");
+                    console.log(data);
+                    $window.location.href = '/login';
+                }).error(function (err) {
+                    alert("Unable to connect to the server.");
+                });
+            }
+        });
