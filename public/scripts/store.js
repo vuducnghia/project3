f2e6292fecee5aa1f003c90125b50931f91df1cf
thangbk2209 <thangbk2209@gmail.com>
2017-11-23 09:42:42 +0700

    fix client login khách hàng

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -30,13 +30,44 @@ console.log('start');
                 });
             }
         });
+         app.controller('dang_nhap', function ($http,$scope,$window) {
+            
+            $scope.dangNhap = function () {
+                console.log('lol');
+                console.log($scope);
+                var data = {
+                    username : $scope.username,
+                    password: $scope.password
+                };
+
+                console.log(data);
+                console.log(data);
+                $http({
+                    method: "POST",
+                    url: "users/login",
+                    data: data
+                }).success(function (data) {
+                    
+                    console.log("Login successful");
+                    console.log(data);
+                }).error(function (err) {
+                    alert("Unable to connect to the server.");
+                });
+            }
+        });
         app.controller("loaiSanPham", function ($scope, $http) {
             $http.get("/category").then(function(result) {
                 console.log('startaaa');
                 console.log(result);
                 $scope.categories = result.data;
-                
-            }); 
-           
-         
+            });  
+
+            $scope.cat = function(id){
+                console.log(id);
+                $http.get("/category/{{id}}").then(function(result) {
+                    console.log('startaaa');
+                    console.log(result);
+                    $scope.categories = result.data;
+            });  
+            }
         })
