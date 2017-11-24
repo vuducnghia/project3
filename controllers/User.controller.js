f79c86c581954734fd8be9a779b20f58ff805f05
Thanh Tien <tienthanh100696@gmail.com>
2017-11-24 14:52:13 +0700

    Logout

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -89,13 +89,13 @@ exports.login = (req, res, next) => {
   //     // return res.redirect('/');
   //   });
   // })(req, res, next);
-  console.log('current user: ', req.user);
+  console.log('login controller');
+  // console.log('current user: ', req.user);
   res.json({user: req.user});
 
 
 }
 exports.logout = (req, res) => {
   req.logout();
-  console.log('current user: ', req.user);
-  res.redirect('/');
+  res.json({msg: 'You are out of my system!'});
 }
