c95680a4119a09b7eb8a542bd3d8bc8ea8adc461
Vũ Đức Nghĩa <vuducnghia1996@gmail.com>
2017-11-25 16:16:32 +0700

    phan lai cau truc thu mua admin

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -4,15 +4,28 @@ var adminController = require('../controllers/admin/account.controller');
 var poolConnection = require('../models/pool.connection');
 var passport = require('passport');
 
+// router client
+router.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.render('admin/home', {title: 'Express'});
+});
 
 router.get('/sign_up', function (req, res, next) {
   res.setHeader('Content-Type', 'text/html');
   res.render('admin/account/sign_up', {title: 'Express'});
 });
+
 router.get('/login', function (req, res, next) {
   res.setHeader('Content-Type', 'text/html');
   res.render('admin/account/login', {title: 'Express'});
 });
+
+
+
+
+
+// router backend
+
 
 router.post('/sign_up', adminController.sign_up);
 router.post('/login', passport.authenticate('local-admin', { failureRedirect: '/admin/login' }), adminController.login);
