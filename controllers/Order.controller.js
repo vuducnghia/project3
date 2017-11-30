924cb678f96a819cccdcaa530c36fa19417def30
Thanh Tien <tienthanh100696@gmail.com>
2017-11-30 17:18:23 +0700

    Save order to database

--- controllers/Order.controller.js
+++ controllers/Order.controller.js
@@ -1,13 +1,59 @@
 var Order = require('../models/Order.model');
+var poolConnection = require('../models/pool.connection');
 
 exports.makeOrder = (req, res) => {
-  console.log('Gio Hang: ', req.body.giohang);
+  console.log('user: ', req.user);
   const giohang = req.body.giohang;
+  const thongTinNguoiNhan = req.body.thongTinNguoiNhan;
+  const storeIdsCatched = [];
   const storeIds = giohang.map((item) => {
     return item.idCuaHang;
-  }).filter((elem, index, self) => {
-    return index == self.indexOf(elem);
-  });
+  }).filter((item) => {
+    let isCatched = storeIdsCatched.indexOf(item) > -1 ? true : false;
+    if(isCatched) {
+      return false;
+    }
+    storeIdsCatched.push(item);
+    return true;
+  })
   console.log('storeIds: ', storeIds);
+
+  if(!storeIds || !storeIds.length) {
+    console.log("Thieu id cua store!!");
+    return res.json({err_msg: "Thieu id cua store!!"});
+  }
+  storeIds.forEach((storeId) => {
+    const createdDate = Date();
+    const orderInfo = {
+      create_date: createdDate,
+      storeId: storeId,
+      totalPrice: 0,
+      customerInfo: {
+        name: thongTinNguoiNhan.tenNguoiNhan,
+        phone: thongTinNguoiNhan.sdtNguoiNhan,
+        address: thongTinNguoiNhan.noiNhan
+      },
+      items: [
+      ]
+    }
+    giohang.forEach((item) => {
+      if(storeId == item.idCuaHang) {
+        orderInfo.items.push({
+          idProduct: item.idSanPham,
+          idStore: item.idCuaHang,
+          quantity: item.number
+        });
+        orderInfo.totalPrice += item.number*item.price;
+      }
+    })
+    Order.makeOrder(orderInfo, req.user, (err, order) => {
+      if(err) {
+        console.log(err);
+        res.json({err_msg: "Some thing wrong when make order!!!"})
+      }
+      console.log('Id cua order moi tao: ', order);
+    })
+
+  })
   return res.json({a: 'aaaaaa'});
 }
